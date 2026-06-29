332ecdc6fb3ee430a06ef822af10efeb360bac84
sdel4030 <sdel4030@gmail.com>
2026-06-29 23:35:41 +0330

    Refactor IPostMeta interface methods and documentation

--- src/WordPress/IPostMeta.php
+++ src/WordPress/IPostMeta.php
@@ -5,46 +5,26 @@
  */
 interface WpTesting_WordPress_IPostMeta
 {
-
     /**
      * Retrieve post meta field for a post.
      *
-     * @since 1.5.0
-     * @uses $wpdb
-     * @link http://codex.wordpress.org/Function_Reference/get_post_meta
-     *
      * @param string $key The meta key to retrieve.
-     * @return mixed Will be an array if $single is false. Will be value of meta data field if $single
-     *  is true.
+     * @return mixed Will be an array if $single is false. Will be value of meta data field if $single is true.
      */
     public function getCurrentPostMeta($key);
 
     /**
      * Retrieve post meta field for a post.
      *
-     * @since 1.5.0
-     * @uses $wpdb
-     * @link http://codex.wordpress.org/Function_Reference/get_post_meta
-     *
      * @param int $postId Post ID.
      * @param string $key The meta key to retrieve.
-     * @param bool $isSingle Whether to return a single value.
-     * @return mixed Will be an array if $single is false.
-     *               Will be value of meta data field if $single is true.
+     * @param bool $isSingle Whether to return a single value. Default false.
+     * @return mixed Will be an array if $single is false. Will be value of meta data field if $single is true.
      */
-    public function getPostMeta($postId, $key, $isSingle);
+    public function getPostMeta($postId, $key, $isSingle = false);
 
     /**
      * Update post meta field based on post ID.
-     *
-     * Use the $prev_value parameter to differentiate between meta fields with the
-     * same key and post ID.
-     *
-     * If the meta field for the post does not exist, it will be added.
-     *
-     * @since 1.5.0
-     * @uses $wpdb
-     * @link http://codex.wordpress.org/Function_Reference/update_post_meta
      *
      * @param int $postId Post ID.
      * @param string $key Metadata key.
