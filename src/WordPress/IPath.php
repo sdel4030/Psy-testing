fc98c0f94e8233db07db5bc76f183ff646c8fae1
sdel4030 <sdel4030@gmail.com>
2026-06-29 23:36:20 +0330

    Fix typos in IPath interface documentation

--- src/WordPress/IPath.php
+++ src/WordPress/IPath.php
@@ -1,36 +1,32 @@
 <?php
 
 /**
- * Knows about WordPress pathes and directories
+ * Knows about WordPress paths and directories
  */
 interface WpTesting_WordPress_IPath
 {
-
     /**
      * Absolute path to the WordPress directory.
-     * @return string
+     * * @return string
      */
     public function getAbsPath();
 
     /**
      * Allows for the plugins directory to be moved from the default location.
      *
-     * @since 2.6.0
      * @return string
      */
     public function getPluginDir();
 
     /**
-     * Determines a writable directory for temporary files (with trailing slahs added).
-     *
-     * @since 2.5.0
+     * Determines a writable directory for temporary files (with trailing slash added).
      *
      * @return string Writable temporary directory
      */
     public function getTempDir();
 
     /**
-     * Where WordPress holds it's public content
+     * Where WordPress holds its public content
      *
      * @return string
      */
@@ -41,11 +37,6 @@ interface WpTesting_WordPress_IPath
      *
      * Will remove trailing forward and backslashes if it exists already before adding
      * a trailing forward slash. This prevents double slashing a string or path.
-     *
-     * The primary use of this is for paths and thus should be used for paths. It is
-     * not restricted to paths and offers no specific path support.
-     *
-     * @since 1.2.0
      *
      * @param string $path What to add the trailing slash to.
      * @return string String with trailing slash added.
