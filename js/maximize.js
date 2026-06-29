506a960ed394e19e4b58b7983a0bb656a789cca6
sdel4030 <sdel4030@gmail.com>
2026-06-29 22:51:57 +0330

    Modernize jQuery.wptMaximize Plugin for WordPress
    Updated the jQuery.wptMaximize plugin to modern standards by replacing deprecated methods, optimizing DOM references, and enhancing icon structure.

--- js/maximize.js
+++ js/maximize.js
@@ -1,46 +1,55 @@
+/**
+ * Modernized jQuery.wptMaximize Plugin for WordPress 7+
+ * Replaces deprecated .bind(), updates DOM body reference, and cleans up ancient TinyMCE skin hooks.
+ */
 (function ($) {
+    'use strict';
 
-    function maximizeItem(maximizable, settings) {
-        var container    = $('body:first'),
-            scrollable   = maximizable.find('.inside:first'),
-            toggleButton = $('<button/>'),
-            buttonText   = $('<span/>').addClass('screen-reader-text').prependTo(toggleButton),
+    function maximizeItem($maximizable, settings) {
+        // ۱. استفاده از ارجاع مستقیم و سریع‌تر به بادی مرورگر
+        var $body        = $(document.body),
+            $scrollable  = $maximizable.find('.inside:first'),
+            $toggleButton = $('<button/>'),
+            $buttonText   = $('<span/>').addClass('screen-reader-text').appendTo($toggleButton),
             isMaximized  = false;
 
         function changeLabel() {
             var label = isMaximized ? settings.minimizeLabel : settings.maximizeLabel;
-            toggleButton.attr('title', label);
-            buttonText.text(label);
-        };
+            $toggleButton.attr('title', label);
+            $buttonText.text(label);
+        }
 
-        function toggleMaximize() {
+        function toggleMaximize(e) {
+            e.preventDefault(); // مهار لرزش یا رفتارهای ناخواسته دکمه در سابمیت‌های ادمین
             isMaximized = !isMaximized;
 
             changeLabel();
 
-            container.toggleClass('wpt_maximize', isMaximized);
-            maximizable.toggleClass('wpt_maximized', isMaximized);
-            toggleButton.toggleClass('active', isMaximized);
+            // جابجایی هوشمند کلاس‌ها برای مدیریت تمام‌صفحه در CSS
+            $body.toggleClass('wpt_maximize', isMaximized);
+            $maximizable.toggleClass('wpt_maximized', isMaximized);
+            $toggleButton.toggleClass('active', isMaximized);
 
-            return false;
-        };
+            // تریگر کردن رویداد ریسایز برای بازتنظیم چارت‌ها در حالت تمام‌صفحه
+            $(window).trigger('resize.wptDiagram');
+        }
+
         changeLabel();
 
-        maximizable.addClass('wpt_maximizable');
-        scrollable.addClass('wpt_scroll');
+        // افزودن کلاس‌های ساختاری افزونه
+        $maximizable.addClass('wpt_maximizable');
+        $scrollable.addClass('wpt_scroll');
 
-        toggleButton
-            .bind('click', toggleMaximize)
-            .attr('type', 'button').addClass('handlediv button-link qt-dfw')
-            .insertBefore(maximizable.find('.handlediv:first'))
-        ;
+        // ۲. اصلاح کلیدی: جایگزینی .bind() با .on() و الحاق دکمه به بالای متاباکس وردپرس
+        $toggleButton
+            .attr('type', 'button')
+            .addClass('handlediv button-link qt-dfw wpt-toggle-fullscreen')
+            .on('click.wptMaximize', toggleMaximize)
+            .insertBefore($maximizable.find('.handlediv:first'));
 
-        /* Old WP versions */
-        if (typeof tinyMCE !== 'undefined') {
-            toggleButton.addClass('wp_themeSkin')
-            $('<span/>').addClass('mceIcon mce_fullscreen').prependTo(toggleButton)
-        }
-    };
+        // ۳. مدرن‌سازی ساختار آیکون‌ها: استفاده از فونت‌آیکون استاندارد ادمین وردپرس (Dashicons)
+        $('<span/>').addClass('dashicons dashicons-editor-expand').prependTo($toggleButton);
+    }
 
     $.fn.wptMaximize = function(options) {
         var settings = $.extend({
